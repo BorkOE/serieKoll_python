10ef90f6a57789e08d8bdd5d90a30f2f8b4d7dec
Oskar Bork <o.e.bork@gmail.com>
2023-05-06 14:00:31 +0200

    Ändrad till csv-fil, övriga förbättringar

--- serieKoll.py
+++ serieKoll.py
@@ -1,101 +1,89 @@
-# py
-# INSTRUKTIONER: Lägg programmet i mappen med videofiler. En excelfil skapas automatiskt som håller koll på vilka 
-# avsnitt som tittats på. Starta programmet för att fortsätta på rätta avsnitt. 
-
-# Detta är en stabil build 
+'''
+INSTRUKTIONER: Lägg programmet i mappen med videofiler. En csv skapas automatiskt som håller koll på vilka 
+avsnitt som tittats på. Starta programmet för att fortsätta på rätta avsnitt. 
+'''
 
 from pathlib import Path
-import openpyxl, os, datetime, time, statistics, pyinputplus
+import os, statistics, pyinputplus
+import csv
+
+def read_csv_to_dict(file_path):
+    with open(file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = {col: [] for col in csv_reader.fieldnames}
+        for row in csv_reader:
+            for col in csv_reader.fieldnames:
+                data[col].append(row[col])
+        return data
+
+def write_dict_to_csv(file_path, data_dict):
+    with open(file_path, 'w', newline='') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=data_dict.keys())
+        csv_writer.writeheader()
+        for i in range(len(data_dict[list(data_dict.keys())[0]])):
+            row = {col: data_dict[col][i] for col in data_dict.keys()}
+            csv_writer.writerow(row)
 
 # Folder för videos
-folder = os.path.dirname(os.path.realpath(__file__)) # Få mappen där filen körs ifrån
-folder = Path(folder)
+folder = Path(os.getcwd())
 
 # Läs in filer
 filer = list(folder.glob('*')) # Lägger filnamnen i en lista
 
-# ändra CWD 
-os.chdir(folder)
+filändelse = [f.suffix for f in filer if f.suffix]
+vanligasteFiltyp = statistics.mode(filändelse)  # Ta fram vanligaste filtyp
+allaFiltyper = set(filändelse)  # alla filtyper
 
-# Sätt upp listor
-videoLista = [] 
-filändelseLista = []
-allaFiltyper = []
+if os.path.exists(f'{folder.name}.csv'):
+    print('Hittade sparfil!')
+    tittadeVideos = read_csv_to_dict(f'{folder.name}.csv').get('avklarade')
+    print('csv laddad...')
+    filtyp = Path(tittadeVideos[0]).suffix
 
-# loopar över filer för att kolla filändelser
-for filnamn in filer:
-    
-    filändelse = '.' + str(filnamn.name).split('.')[-1]
-    filändelseLista.append(filändelse) # Lägger till alla filändelser i en lista 
-
-vanligasteFiltyp = statistics.mode(filändelseLista) # Ta fram vanligaste filtyp
-allaFiltyper = list(set(filändelseLista)) # Skapa en lista med alla filtyper
-
-if '.xlsx' in allaFiltyper:   # Om det redan finns en excelfil
-    print('Hittade excelfil!')
-    wb = openpyxl.load_workbook(str(folder.name) + '.xlsx')
-    print('Excelfil laddad...')
-    sheet = wb.active
-    filtyp = '.' + sheet.cell(column=1, row=2).value.split('.')[-1]
-    print(filtyp)
-
-else: # Om det inte finns en excelfil
+else: # Om det inte finns en fil
+    tittadeVideos = []
     print('Det verkar som att du tittar på den här serien första gången...')
     print('Är videon av filtypen: '+ str(vanligasteFiltyp) + '?')
     svar = pyinputplus.inputMenu(['Ja', 'Nej'], numbered=True)
 
     if svar == 'Ja':
-        filtyp = '.' + str(vanligasteFiltyp)
+        filtyp = vanligasteFiltyp
     else:
         print('Vilken filtyp är videofilerna av?')
         svar = pyinputplus.inputMenu(allaFiltyper, numbered=True)
-        filtyp = '.' + str(svar)
+        filtyp = str(svar)
 
-    print('Skapar ny excelfil')
-    wb = openpyxl.Workbook() # Skapar ny workbook
 
 # loopar över filer
-for filnamn in filer:
-    if str(filnamn.name).split('.')[-1] in filtyp: # Letar efter filändelsen
-        videoLista.append(str(filnamn))    #  Lägger till i en videolista # TODO Är det här jag ska plocka hela path istället?
+videoLista = [f.name for f in filer if f.suffix == filtyp]
 
 # Starta while-loop här
 while True:
-    # Kolla igenom första kolumnen 
-    sheet = wb.active
-    tittadeVideos = []
-    for row in range(1,sheet.max_row+1):  # Loopar över raderna i kolunmn 1
-        tittadeVideos.append(sheet.cell(row=row, column=1).value)      # Lägger filnamnen i kolumn 1 i en lista
-
-    if tittadeVideos != [None]: # Om listan med tittade videos inte är tom:
-        for i in tittadeVideos:  # Loopa över tittade videos
-            if i in videoLista: 
-                print(str(i) +' har du redan sett. Jag tar fram nästa video...') # Plockar ut filnamnet från filepath 
-                videoLista.remove(videoLista[0])     # Ta bort först elementet i listan med alla videos 
+    if tittadeVideos: 
+        for v in tittadeVideos:  # Loopa över tittade videos
+            if v in videoLista: 
+                print(str(v) +' har du redan sett. Jag tar fram nästa video...') # Plockar ut filnamnet från filepath 
+                videoLista.remove(videoLista[0])                                 # Ta bort först elementet i listan med alla videos 
     else:
         print('Du har inte sett några videos än...')
         print('Startar första videon...')
 
     körFil = videoLista[0] # Plocka ut första elementet i videoListan som körfil.
     
-    print()
-    print('Körfilen är: '+ körFil)
+    print('\nKörfilen är: '+ körFil)
     input('Tryck enter när du är redo...')
     # Lägg till den körda filen i excelfilen
-    sheet.cell(row=sheet.max_row+1, column=1).value = str(körFil)
 
     # Kör igång filen. OBS! Koden väntar på att videofönstret stängs innan den fortsätter.  
-    print()
-    print('Kör igång ' + str(körFil.split('\\')[-1]))
-    print()
-    print('OBS! Stäng inte det här fönstret för då blir sparfilen korrupt.')
-    os.system('"' + körFil + '"') 
-    
-    print('Sparar excelfil...')
-    wb.save(str(folder.name) + '.xlsx')
+    res = os.system('"' + körFil + '"') 
+    if res != 0:    # Felmeddelande mac os
+        res = os.system('open "' + körFil + '"') 
 
-    print()
-    print('Vill du se ett avsnitt till?')
+    print('Sparar fil...')
+    tittadeVideos.append(körFil)
+    write_dict_to_csv(f'{folder.name}.csv', {'avklarade':tittadeVideos})
+
+    print('\n\nVill du se ett avsnitt till?')
     slutMeny = ['Spela en till', 'Avsluta']
     slutVal = pyinputplus.inputMenu(slutMeny, numbered=True)
     if slutVal == 'Avsluta':
